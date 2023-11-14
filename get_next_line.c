7ea04eb5505507218bbf1ac221efd4f37e10c53d
Ismael Henrique Da Silva <ishenriq@c2r3p5.42sp.org.br>
2023-11-13 21:01:02 -0300

    terminado o mandatório

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,20 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 20:20:19 by ishenriq          #+#    #+#             */
-/*   Updated: 2023/11/13 19:04:03 by ishenriq         ###   ########.org.br   */
+/*   Updated: 2023/11/13 20:58:29 by ishenriq         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-void	ft_lstclear(t_list **lst, int size)
+static void	ft_lstclear(t_list **lst, int size)
 {
 	t_list	*next;
 	t_list	*head;
 
 	if (!lst || size == 0)
 		return ;
-
 	head = *lst;
 	next = *lst;
 	if (size == -1)
@@ -42,10 +41,10 @@ void	ft_lstclear(t_list **lst, int size)
 	*lst = head;
 }
 
-int	ft_construct_list(char *str, t_list **head, int size_line)
+static int	ft_construct_list(char *str, t_list **head, int size_line)
 {
-	int	i;
-	int	out;
+	int		i;
+	int		out;
 	t_list	*content;
 
 	out = 0;
@@ -61,17 +60,17 @@ int	ft_construct_list(char *str, t_list **head, int size_line)
 	return (out);
 }
 
-char	*ft_build_str(t_list **head)
+static char	*ft_build_str(t_list **head)
 {
 	char	*gnl;
-	int	i;
-	int	len_list_until_nl;
+	int		i;
+	int		len_list_until_nl;
 
 	if (!*head)
 		return (NULL);
 	len_list_until_nl = ft_lstsize(*head);
 	i = 0;
-	gnl = malloc((len_list_until_nl + 1) * sizeof(char));	
+	gnl = malloc((len_list_until_nl + 1) * sizeof(char));
 	if (gnl == 0)
 		return (0);
 	while (len_list_until_nl--)
@@ -84,38 +83,42 @@ char	*ft_build_str(t_list **head)
 	return (gnl);
 }
 
-char	*get_next_line(int fd)
+static int	whiling(int fd, char **buffer, t_list **head)
 {
-	char	*buffer;
-	static t_list	*head;
 	int	out;
-	int	len;
-	char	*str;
-	int read_i;
+	int	read_i;
 
 	out = 0;
-	read_i = 0;
-	str = 0;
+	while (out == 0)
+	{
+		read_i = read(fd, *buffer, BUFFER_SIZE);
+		if (fd < 0 || read_i < 0 || BUFFER_SIZE <= 0)
+		{
+			ft_lstclear(head, -1);
+			free(*buffer);
+			return (0);
+		}
+		if (read_i == 0)
+			break ;
+		if (ft_construct_list(*buffer, head, read_i) == 1)
+			out = 1;
+	}
+	return (1);
+}
+
+char	*get_next_line(int fd)
+{
+	char			*buffer;
+	static t_list	*head;
+	char			*str;
+
 	buffer = malloc(BUFFER_SIZE * sizeof(char));
 	if (buffer == 0)
 		return (0);
-	while (out == 0)
-	{
-		if (fd < 0 || read_i < 0 || BUFFER_SIZE <= 0)
-		{
-			ft_lstclear(&head, -1);
-			free(buffer);
-			return (NULL);
-		}
-		read_i = read(fd, buffer, BUFFER_SIZE);
-		if (read_i == 0)
-			break ;
-		if (ft_construct_list(buffer, &head, read_i) == 1)
-			out = 1;
-	}
-	len = ft_lstsize(head);
+	if (!whiling(fd, &buffer, &head))
+		return (NULL);
 	str = ft_build_str(&head);
-	ft_lstclear(&head, len);
+	ft_lstclear(&head, ft_lstsize(head));
 	free(buffer);
 	return (str);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,30 +6,30 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 20:21:26 by ishenriq          #+#    #+#             */
-/*   Updated: 2023/11/11 18:22:39 by ishenriq         ###   ########.org.br   */
+/*   Updated: 2023/11/13 19:39:00 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1
-# endif
+# include <unistd.h>
+# include <stdlib.h>
 
-#include <unistd.h>
-#include <stdlib.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
 
 typedef struct s_list
 {
-	char	content;
+	char			content;
 	struct s_list	*next;
-} t_list;
+}	t_list;
 
 char	*get_next_line(int fd);
 t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
-int	ft_lstsize(t_list *lst);
+int		ft_lstsize(t_list *lst);
 
 #endif
